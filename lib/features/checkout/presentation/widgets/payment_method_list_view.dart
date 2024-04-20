7cef249ab3600103c49ee0bee7b32c970ea4bad7
soudyjr <elsoudy222@gmail.com>
2024-04-20 22:58:28 +0400

    create payment method list view

---
+++ lib/features/checkout/presentation/widgets/payment_method_list_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:payment/features/checkout/presentation/widgets/payment_method_item.dart';
+
+class PaymentMethodsListView extends StatefulWidget {
+  const PaymentMethodsListView({
+    super.key,
+   // required this.updatePaymentMethod,
+  });
+
+//  final Function({required int index}) updatePaymentMethod;
+
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
+  final List<String> paymentMethodsItems = const [
+    'assets/svg/card.svg',
+    'assets/svg/paypal.svg',
+    "assets/svg/apple_pay.svg",
+  ];
+
+  int activeIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 62,
+      child: ListView.builder(
+          itemCount: paymentMethodsItems.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: GestureDetector(
+                onTap: () {
+                  activeIndex = index;
+                  setState(() {});
+                 // widget.updatePaymentMethod(index: activeIndex);
+                },
+                child: PaymentMethodItem(
+                  isActive: activeIndex == index,
+                  image: paymentMethodsItems[index],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
